b67b6fd941e7c8ee5725416a971a5e5a6230c44a
Shadi Abdelslam <shadodev@gmail.com>
2024-01-03 00:04:40 +0200

    Update home page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,6 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-      <h1 className='main-dev'>Hello World</h1>
+      <h1 className='main-dev'>Hello World Next.js</h1>
   )
 }
